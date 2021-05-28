5f7c2a8ab8c02e806fbcae5e74f31b7a68d78d48
Juned Memon <juned.memon@hitachivantara.com>
2021-05-28 18:25:51 +0530

    added java code

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM maven:3.6.3-jdk-11-slim AS build
+RUN mkdir -p /workspace
+WORKDIR /workspace
+COPY pom.xml /workspace
+COPY src /workspace/src
+RUN mvn -B -f pom.xml clean package -DskipTests
+
+ARG REAN_PLATFORM_USER=reanplatform
+ARG REAN_PLATFORM_GROUP=reanplatform
+ARG REAN_PLATFORM_HOME=/opt/${REAN_PLATFORM_USER}
+
+FROM openjdk:11-jdk-slim
+COPY --from=build /workspace/target/*.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","app.jar"]
+
+HEALTHCHECK --interval=5s --timeout=3s --start-period=50s --retries=3 \
+    CMD curl -s http://localhost:8080 || exit 1
---
+++ src/test/java/com/rean/sampleapplication/controller/AppControllerTest.java
@@ -0,0 +1,84 @@
+package com.rean.sampleapplication.controller;
+
+import static org.mockito.Matchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.rean.sampleapplication.config.TestUtils;
+import com.rean.sampleapplication.model.User;
+import com.rean.sampleapplication.service.UserService;
+
+@WebMvcTest(value = RestApiController.class, secure = false)
+public class AppControllerTest {
+
+	@ClassRule
+	public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+	@Rule
+	public final SpringMethodRule springMethodRule = new SpringMethodRule();
+
+	@MockBean
+	private UserService userService;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	public AppControllerTest() {
+
+	}
+	
+	@Test
+	public void getHomePage() throws Exception {
+
+		List<User> userList = new ArrayList<>();
+		User mockedUser = new User();
+		mockedUser.setName("Lily");
+		mockedUser.setAge(26);
+		mockedUser.setSalary(50000);
+		userList.add(mockedUser);
+
+		Mockito.when(userService.findAllUsers()).thenReturn(userList);
+
+		RequestBuilder requestBuilder = TestUtils.getGetRequestBuilder("/");
+		mockMvc.perform(requestBuilder)
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			//.andExpect(MockMvcResultMatchers.content().json("[{\"name\":\"Lily\",\"age\":26,\"salary\":50000}]"))
+			.andReturn();
+
+	}
+	
+	@Test
+	public void getPartialPage() throws Exception {
+
+		List<User> userList = new ArrayList<>();
+		User mockedUser = new User();
+		mockedUser.setName("Lily");
+		mockedUser.setAge(26);
+		mockedUser.setSalary(50000);
+		userList.add(mockedUser);
+
+		Mockito.when(userService.findAllUsers()).thenReturn(userList);
+
+		RequestBuilder requestBuilder = TestUtils.getGetRequestBuilder("/partials/list");
+		mockMvc.perform(requestBuilder)
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			//.andExpect(MockMvcResultMatchers.content().json("[{\"name\":\"Lily\",\"age\":26,\"salary\":50000}]"))
+			.andReturn();
+
+	}
+
+}
